799f2e21ff617acc38b83af4b8f660d227113859
Markus Heck <markus@heck-consulting.de>
2025-05-27 17:21:53 +0200

    Test

--- src/main/java/de/heckconsulting/heck_test/HeckTestApplication.java
+++ src/main/java/de/heckconsulting/heck_test/HeckTestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HeckTestApplication {
 
 	public static void main(String[] args) {
+		// Test
 		SpringApplication.run(HeckTestApplication.class, args);
 	}
 
